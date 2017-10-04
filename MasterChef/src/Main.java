4117973b1b409e5e13b3c3a824edca28ac84e80a
lanechanger <jeremy_ng_86@live.ca>
2017-10-04 00:48:55 -0400

    Added test sample soln as a first test

---
+++ MasterChef/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		java.io.BufferedReader reader = new java.io.BufferedReader (new java.io.InputStreamReader (System.in));
+		String input;
+		
+		while (!(input=reader.readLine()).startsWith("42")) { 
+			System.out.println(input);
+		}
+	}
+}
