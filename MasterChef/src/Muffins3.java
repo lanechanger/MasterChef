9d50643e342ae79f48659d0c72d81dee1aa992e1
lanechanger <jeremy_ng_86@live.ca>
2017-10-25 18:12:35 -0400

    Uploaded solution to Muffins3

--- MasterChef/src/Muffins3.java
+++ MasterChef/src/Muffins3.java
@@ -3,7 +3,7 @@ import java.lang.Exception;
 /* https://www.codechef.com/problems/MUFFINS3
  * Practice --> Beginner --> Packaging Cupcakes
  */
-public class Main {
+public class Muffins3 {
 	public static void main (String[] args) throws Exception
 	{
 		Scanner inputs = new Scanner (System.in);
@@ -11,6 +11,9 @@ public class Main {
 		
 		for(int i = 0; i < T ; i++) {
 			int N = inputs.nextInt();
+			// initially went with a static method that looped & looked for the biggest value A from 1 to N
+			// and determined which A had the biggest left over = N % A and returned that A
+			// but that was too slow and all the answers turned out to be N/2 + 1 anyways
 			System.out.println((N/2)+1);
 		}
 		
