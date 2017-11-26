f01a07a36fb40c4e8b910af13b0d7c65e6cf79ea
lanechanger <jeremy_ng_86@live.ca>
2017-11-25 23:41:27 -0500

    Updated Error and added a JUnit for it

--- MasterChef/src/Error.java
+++ MasterChef/src/Error.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
+
 /* https://www.codechef.com/problems/ERROR
  * Practice --> Easy --> Chef and Feedback
  */
 class Error {
-	public static void main (String[] args)
-	{
-		Scanner inputs = new Scanner (System.in);
+	public static void main(String[] args) {
+		Scanner inputs = new Scanner(System.in);
 		int T = inputs.nextInt();
 
-		while(T --> 0) {
+		while (T-- > 0) {
 			String feedback = inputs.next();
-			
-			if(feedback.contains("010") || feedback.contains("101")) {
-				System.out.println("Good");
-			}else {
-				System.out.println("Bad");
-			}			
-		}
+
+			System.out.println(judge(feedback));
+		}		
 
 		inputs.close();
-	} 
+	}
+
+	static String judge(String feedback) {
+		if (feedback.contains("010") || feedback.contains("101")) {
+			return "Good";
+		} else {
+			return "Bad";
+		}
+	}
 }
---
+++ MasterChef/test/ErrorTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ErrorTest {
+
+	@Test
+	public void testGood1() {
+		// setup
+		String s1 = "101";
+	
+		// execute
+		String rslt = Error.judge(s1);
+		
+		// assert
+		String expected = "Good";
+		assertEquals(rslt,expected);
+	}
+	
+	@Test
+	public void testGood2() {
+		// setup
+		String s1 = "010";
+	
+		// execute
+		String rslt = Error.judge(s1);
+		
+		// assert
+		String expected = "Good";
+		assertEquals(rslt,expected);
+	}
+	
+	@Test
+	public void testGood3() {
+		// setup
+		String s1 = "011";
+	
+		// execute
+		String rslt = Error.judge(s1);
+		
+		// assert
+		String expected = "Bad";
+		assertEquals(rslt,expected);
+	}
+
+}
